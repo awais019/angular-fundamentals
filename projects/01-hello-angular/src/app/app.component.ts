3853621f6e71ee7ee5dd09dab1e75ac5247ce804
Muhammad Awais <muhammad.awais.20024@gmail.com>
2024-05-13 10:14:48 +0500

    Creating a component

--- projects/01-hello-angular/src/app/app.component.ts
+++ projects/01-hello-angular/src/app/app.component.ts
@@ -7,9 +7,15 @@ import { RouterOutlet } from '@angular/router';
   standalone: true,
   imports: [CommonModule, RouterOutlet],
   template: `
-    <h1>If you are reading this...</h1>
-    <p>Things have worked out well! 🎉</p>
+    <ol>
+      <li>Harry Potter</li>
+      <li>Inception</li>
+      <li>Interstellar</li>
+    </ol>
   `,
-  styles: ``,
+  styles: `
+  ol{
+    list-style-type: upper-roman
+  }`,
 })
 export class AppComponent {}
