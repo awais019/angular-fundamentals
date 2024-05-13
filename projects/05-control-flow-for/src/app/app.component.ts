043a9e04730ba15e37d46f27c66abc6d68f9b495
Muhammad Awais <muhammad.awais.20024@gmail.com>
2024-05-13 20:47:33 +0500

    Flow control with @for

--- projects/05-control-flow-for/src/app/app.component.ts
+++ projects/05-control-flow-for/src/app/app.component.ts
@@ -6,31 +6,35 @@ import { Component } from '@angular/core';
   template: `
     <section class="container">
       <!-- This article element represents and entire listing -->
+      @for (car of carList; track car) {
       <article class="listing">
         <div class="image-parent">
           <img class="product-image" src="https://placehold.co/100x100" />
         </div>
         <section class="details">
-          <p class="title"><!-- car make and model--></p>
+          <p class="title">{{ car.make }} {{ car.model }}</p>
           <hr />
           <p class="detail">
             <span>Year</span>
-            <span><!-- year --></span>
+            <span>{{ car.year }}</span>
           </p>
           <div class="detail">
             <span>Transmission</span>
-            <span><!-- transmission --></span>
+            <span>{{ car.transmission }}</span>
           </div>
           <p class="detail">
             <span>Mileage</span>
-            <span><!-- miles --></span>
+            <span>{{ car.miles }}</span>
           </p>
           <p class="detail">
             <span>Price</span>
-            <span><!-- price --></span>
+            <span>{{ car.price }}</span>
           </p>
         </section>
       </article>
+      } @empty {
+      <p>No listings available</p>
+      }
     </section>
   `,
   styleUrl: 'app.component.css',
